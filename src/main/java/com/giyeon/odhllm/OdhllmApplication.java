131766a11ceca60006867f09e5ab3fc40554bc12
GiyeonChoi <bdcgy22@daum.net>
2025-02-02 13:19:12 +0900

    첫 번째 커밋

---
+++ src/main/java/com/giyeon/odhllm/OdhllmApplication.java
@@ -0,0 +1,13 @@
+package com.giyeon.odhllm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdhllmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdhllmApplication.class, args);
+    }
+
+}
